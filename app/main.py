d2f7ce31dc61eff0e769a65ce5a7862bc3facb95
stand4r <sahojkodima888@gmail.com>
2024-02-10 15:38:59 +0300

    2

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ import smtplib
 from passlib.context import CryptContext
 import random
 from pydantic import BaseModel, ValidationError
+from datetime import datetime
 
 
 app = FastAPI()
@@ -39,29 +40,31 @@ example_profile = {
 @app.post("/api/auth/login")
 def login(username:str, password:str):
     if not dbase.is_user(username):
-        return -2
+        return "Invallid username or password"
     user = dbase.find_by_username(username)
     if user["password_hash"] == get_password_hash(password):
-        codes[user["username"]] = random.randint(100000, 999999)
-        gmail.send_message("ifs2343234@gmail.com", user["email"], f"Confirmation code\n{codes[user['username']]}")
-        return 0
+        return user
     else:
-        return -1
+        return "Invallid username or password"
 
 @app.post("/api/auth/confirm")
 def confirm(username:str, code:int):
-    if codes[username] == code:
-        return "Success"
+    time = datetime.now() - codes[username][1]
+    if time.minute < 30 and time.hour == 0 and codes[username][0] == code:
+        return dbase.find_by_username(username)
     else:
-        return "Fail"
+        return "Invallid code or more than 30 minutes have passed"
 
 @app.post("/api/auth/register")
 def register(username:str, password:str, email:str, first_name:str, last_name:str):
     if dbase.is_user(username):
-        return -1
+        return "Username exists"
     try:
         user = Profile(4, username, get_password_hash(password), email, {first_name, last_name, "", ""}, [], [])
         dbase.create_user(user)
+        codes[username] = [random.randint(100000, 999999), datetime.now()]
+        gmail.send_message("ifs2343234@gmail.com", user["email"], f"Confirmation code\n{codes[username][0]}")
+        return "Success"
     except Exception as ex:
         print("[register] Some problem...")
         print(ex)
